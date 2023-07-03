6cb94de0dc8e814ff33bae1cf56a54432d3eedb3
Sultan Coskun <sultan.coskun@hfg.design>
2023-07-03 20:57:53 +0300

    coding

---
+++ 17.05/17.05.js
@@ -0,0 +1,17 @@
+//let results;
+//let results2;
+
+//function awerage(x,y) {
+
+//    return (x + y)/2;
+
+///}
+
+//result = awerage(55,66);
+
+//result2 = average(22,6);
+
+
+//console.log('result 1', result);
+
+//console.log('result 2',result2);
